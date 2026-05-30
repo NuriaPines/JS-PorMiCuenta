aaf614713e8847939eb391e6e38372e894fe54d9
Nuria Pinés <nuriapines@gmail.com>
2026-05-30 12:42:08 +0200

    Preparando ejercicio

---
+++ ejerciciosRepaso/lvl3/03-String/Basicos.js
@@ -0,0 +1,12 @@
+//Verificar palabra: Una función que reciba una frase y una palabra, y devuelva true o false si 
+// la contiene.
+
+
+//Invertir texto: Una función que reciba "Nuria" y devuelva "airuN".
+
+
+//Palabra más larga: Una función que reciba una frase y devuelva cuál es la palabra con más letras.
+
+
+//Máscara de texto: Una función que reciba una tarjeta de crédito (ej: "1234567890123456") 
+// y devuelva todo oculto con asteriscos menos los últimos 4 números (ej: "************3456").
